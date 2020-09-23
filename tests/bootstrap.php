92b420db9243b900665151a074c688c91d041c05
Jeffrey Paul <jeffpaul@hotmail.com>
2020-09-23 10:41:29 -0500

    fix: correct base plugin filename in bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -25,7 +25,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/plugin.php';
+	require dirname( dirname( __FILE__ ) ) . '/gutenbridge.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
