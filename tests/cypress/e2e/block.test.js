e3b0976fc7ab6a31fee146a64bf861d07093ae1b
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2023-06-20 16:07:52 +0530

    another attempt at fixing tests

--- tests/cypress/e2e/block.test.js
+++ tests/cypress/e2e/block.test.js
@@ -1,7 +1,7 @@
 describe("Convert test content to blocks", () => {
-    before(() => {
-        cy.login();
-    });
+	before(() => {
+		cy.login();
+	});
 
 	// Ignore Invalid JSON errors.
 	Cypress.on('uncaught:exception', (err, runnable) => {
@@ -12,30 +12,23 @@ describe("Convert test content to blocks", () => {
 		) {
 		  return false;
 		}
-	  });
-
-    it("Create classic post", () => {
-      cy.fixture('example.html').then((contentFixture) => {
-          cy.wpCli("post create --post_title='Classic Post' --post_content='" + contentFixture + "'");
-      })
-  });
+	});
 
 	it('Check blocks have been converted', () => {
-        cy.visit('wp-admin/edit.php');
-        cy.get('#the-list .row-title');
-        cy.contains( '#the-list .row-title', 'Classic Post' ).click();
-        cy.wait(1000);
-        cy.get( 'h2[data-type="core/heading"]' ).should('exist');
-        cy.get( 'h3[data-type="core/heading"]' ).should('exist');
-        cy.get( 'h4[data-type="core/heading"]' ).should('exist');
-        cy.get( 'h5[data-type="core/heading"]' ).should('exist');
-        cy.get( 'h6[data-type="core/heading"]' ).should('exist');
-        cy.get( 'p[data-type="core/paragraph"]' ).should('exist');
-        cy.get( '.wp-block-post-content p[data-type="core/paragraph"] a' ).should('exist');
-        cy.get( 'ul[data-type="core/list"]' ).should('exist');
-        cy.get( 'ol[data-type="core/list"]' ).should('exist');
-        cy.get( 'figure[data-type="core/image"]' ).should('exist');
-        cy.get( 'blockquote[data-type="core/quote"]' ).should('exist');
-    });
-});
+		cy.visit('wp-admin/edit.php');
+		cy.get('#the-list .row-title');
+		cy.contains( '#the-list .row-title', 'Classic Post' ).click();
 
+		cy.get( 'h2[data-type="core/heading"]' ).should('exist');
+		cy.get( 'h3[data-type="core/heading"]' ).should('exist');
+		cy.get( 'h4[data-type="core/heading"]' ).should('exist');
+		cy.get( 'h5[data-type="core/heading"]' ).should('exist');
+		cy.get( 'h6[data-type="core/heading"]' ).should('exist');
+		cy.get( 'p[data-type="core/paragraph"]' ).should('exist');
+		cy.get( '.wp-block-post-content p[data-type="core/paragraph"] a' ).should('exist');
+		cy.get( 'ul[data-type="core/list"]' ).should('exist');
+		cy.get( 'ol[data-type="core/list"]' ).should('exist');
+		cy.get( 'figure[data-type="core/image"]' ).should('exist');
+		cy.get( 'blockquote[data-type="core/quote"]' ).should('exist');
+	});
+});
