5941a7f64f8da176cec437fd668551d455c1dc5e
Darin Kotter <darin.kotter@gmail.com>
2023-10-13 09:52:46 -0600

    Version bump to 1.2.2

--- convert-to-blocks.php
+++ convert-to-blocks.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Convert to Blocks
  * Plugin URI:        https://github.com/10up/convert-to-blocks
  * Description:       Convert classic editor posts to blocks on the fly.
- * Version:           1.2.1
+ * Version:           1.2.2
  * Requires at least: 6.1
  * Requires PHP:      8.0
  * Author:            10up
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags:              gutenberg, block, block migration, gutenberg migration, guten
 Requires at least: 5.7
 Tested up to:      6.3
 Requires PHP:      8.0
-Stable tag:        1.2.1
+Stable tag:        1.2.2
 License:           GPLv2 or later
 License URI:       http://www.gnu.org/licenses/gpl-2.0.html
 
