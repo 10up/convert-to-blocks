cc281abf88ecec50c5cdbbb647ba2f450263c678
Konstantinos Galanakis <kmgalanakis@gmail.com>
2024-04-26 12:09:18 +0300

    Activate plugin before running the tests

--- tests/bin/initialize.sh
+++ tests/bin/initialize.sh
@@ -2,6 +2,7 @@
 wp-env run tests-wordpress chmod -c ugo+w /var/www/html
 wp-env run tests-cli wp rewrite structure '/%postname%/' --hard
 wp-env run tests-cli wp post create ./sample-post.txt  --post_title='Classic Post' --post_status=publish
+wp-env run tests-cli wp plugin activate convert-to-blocks
 
 # Disable gutenberg editor welcome guide modal.
 wp-env run tests-cli wp user meta add admin wp_persisted_preferences '{"core\/edit-post":{"isComplementaryAreaVisible":true,"welcomeGuide":false},"_modified":"2023-10-05T11:26:20.517Z"}' --format=json
